77316386c40c17185a07a7a073881011759c7e3c
Rose <ebrue7949@gmail.com>
2023-04-09 17:05:30 +0300

    axios

---
+++ src/pages/Home.tsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import InputForm from "../components/InputForm";
+import TodoList from "../components/TodoList";
+
+const url = "https://6405db5a40597b65de4320e7.mockapi.io/todos";
+
+interface TodoType {
+  id: string | number;
+  task: string;
+  inDone: boolean;
+}
+
+const Home = () => {
+  const [todos, setTodos] = useState<TodoType[]>([]);
+
+  const getTodos = async () => {
+    try {
+      const { data } = await axios.get(url);
+      setTodos(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="main">
+      <InputForm />
+      <TodoList />
+    </div>
+  );
+};
+
+export default Home;
